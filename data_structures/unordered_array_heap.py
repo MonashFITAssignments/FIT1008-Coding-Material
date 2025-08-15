00d81aba2a008912c14ba28be889c256779ad248
Brendon Taylor <brendon.taylor@monash.edu>
2025-08-15 18:42:06 +1000

    Fixed heap implementation

--- data_structures/unordered_array_heap.py
+++ data_structures/unordered_array_heap.py
@@ -7,19 +7,45 @@ from abc import abstractmethod
 HeapOrders = Literal['min', 'max']
 
 class UnorderedArrayHeap(AbstractHeap[T]):
-    def __init__(self, max_items:int):
+    def __init__(self, max_items:int = 1):
         if not max_items >= 0:
             raise ValueError("Heap must store 0 or more items.")
         self.__array = ArrayR[T](max_items + 1)
         self.__length:int = 0
 
-    def __len__(self) -> int:
-        return self.__length
+    def add(self, item: T) -> None:
+        """ Add an item to the heap.
+        :raises ValueError: if the heap's array is full
+        :complexity best: O(1) the item is adding to the end of the array (no rising required)
+        :complexity worst: O(logN) Need to rise the item to the top of the heap (N is the size of the heap)
+        """
+        if len(self) == len(self.__array) - 1:
+            raise ValueError("Cannot add to full heap.")
+
+        self.__length += 1
+        self.__array[len(self)] = item
+        self._rise(len(self))
+
+    def extract_root(self) -> T:
+        """ Get and remove the root of the heap.
+        :raises: ValueError if the heap is empty
+        :returns: The root of the heap
+        :complexity: O(logN) where N is the size of the heap.
+        Note: Technically there is a best case of O(1) if the items are all the same.
+        """
+        if self.__length == 0:
+            raise ValueError("Cannot extract_root from empty heap.")
+        res = self.__array[1]
+        self.__array[1] = self.__array[len(self)]
+        self.__length -= 1
+        self._sink(1)
+        return res
 
     def peek(self) -> T:
         """ Returns the root of the heap without updating the heap. 
-        raises: ValueError if the heap is empty.
-        returns: The root of the heap
+        :raises: ValueError if the heap is empty.
+        :returns: The root of the heap
+        :complexity: O(1)
         """
         if self.__length == 0:
             raise ValueError("Cannot peek from empty heap.")
@@ -41,9 +67,10 @@ class UnorderedArrayHeap(AbstractHeap[T]):
         
     def __get_child_index(self, k:int) -> int | None:
         """ Returns the index of child of k that would be the parent of the other.
-        returns: None if no child exists, else index of child
+        :returns: None if no child exists, else index of child
+        :complexity: O(1)
         """
-        k2 = k*2
+        k2 = k * 2
         if k2 > len(self):
             return None
         elif k2 == len(self):
@@ -59,14 +86,19 @@ class UnorderedArrayHeap(AbstractHeap[T]):
 
     def __get_parent_index(self, k:int) -> int | None:
         """ Returns the index of the parent of k
-        returns: None if k is the root, else index of k's parent
+        :returns: None if k is the root, else index of k's parent
+        :complexity: O(1)
         """
         if k == 1:
             return None
         return k // 2
 
     def _rise(self, k:int) -> None:
-        """ Rise the element at index k """
+        """ Rise the element at index k
+        N represents the size of the heap
+        :complexity best: O(1) when no rising is required
+        :complexity worst: O(logN) where you need to rize to the top of the heap.
+        """
         rising_item = self.__array[k]
         
         # := is the walrus operator, it assigns the right expression to the left variable, and returns the expression.
@@ -77,19 +109,11 @@ class UnorderedArrayHeap(AbstractHeap[T]):
             
         self.__array[k] = rising_item
 
-    def add(self, item:T) -> None:
-        """ Add an item to the heap.
-        raises: ValueError if the heap's array is full
-        """
-        if len(self) == len(self.__array) - 1:
-            raise ValueError("Cannot add to full heap.")
-        
-        self.__length += 1
-        self.__array[len(self)] = item
-        self._rise(len(self))
-
     def _sink(self, k:int) -> None:
-        """ Sink the element at index k """
+        """ Sink the element at index k
+        :complexity best: O(1) when no sinking is required
+        :complexity worst: O(logN) where you need to sink to the bottom of the heap.
+        """
         sinking_item = self.__array[k]
         while (child_i := self.__get_child_index(k)) is not None and self._should_sink(sinking_item, self.__array[child_i]):
             self.__array[k] = self.__array[child_i]
@@ -97,23 +121,10 @@ class UnorderedArrayHeap(AbstractHeap[T]):
 
         self.__array[k] = sinking_item
 
-    def extract_root(self) -> T:
-        """ Get and remove the root of the heap.
-        raises: ValueError if the heap is empty
-        returns: The root of the heap
-        """
-        if self.__length == 0:
-            raise ValueError("Cannot extract_root from empty heap.")
-        res = self.__array[1]
-        self.__array[1] = self.__array[len(self)]
-        self.__length -= 1
-        self._sink(1)
-        return res
-        
-    def _heapify(heap:UnorderedArrayHeap[T], items:Iterable[T]) -> UnorderedArrayHeap[T]:
+    def _heapify(heap: UnorderedArrayHeap[T], items: Iterable[T]) -> UnorderedArrayHeap[T]:
         """ Construct a heap from an iterable of items. 
-        returns: A heap containing all of the items in the iterable.
-        complexity: O(n) where n is the number of items in the iterable.
+        :returns: A heap containing items in the iterable.
+        :complexity: O(n) where n is the number of items in the iterable.
         """
         try: #call len(iterable) to avoid having to resize a temporary array
             length = len(items)
@@ -146,7 +157,13 @@ class UnorderedArrayHeap(AbstractHeap[T]):
         
         return heap
 
+    def __len__(self) -> int:
+        return self.__length
+
     def __str__(self) -> str:
+        """
+        :complexity: O(n) where n is the number of items in the heap.
+        """
         res = ArrayR(self.__length)
         for i in range(self.__length):
             res[i] = str(self.__array[i + 1])
