a76e6dec87016ea84b76cf772a3e1c3e2280ba71
Brendon Taylor <brendon.taylor@monash.edu>
2025-08-29 14:09:35 +1000

    Minor changes to hash tables

--- data_structures/hash_table_linear_probing.py
+++ data_structures/hash_table_linear_probing.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TypeVar, Tuple
+from typing import TypeVar, Tuple, List
 from data_structures.abstract_hash_table import HashTable
 from data_structures.referential_array import ArrayR
 
@@ -17,6 +17,24 @@ class LinearProbeTable(HashTable[str, V]):
 
     Unless stated otherwise, all methods have O(1) complexity.
     """
+
+    __TABLE_SIZES = [5, 13, 29, 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613, 393241, 786433, 1572869]
+
+    def __init__(self, sizes: None | List[int] = None, hash_base: int | None = 31) -> None:
+        """
+        :param sizes: Optional list of sizes to use for the hash table.
+                      If not provided, a default list of sizes will be used.
+        :complexity: O(1) - Assuming the default sizes are used, we can assume the array is created in O(1) time.
+            If you use this function in any way that passes some variable input for the sizes, then the complexity
+            needs to change accordingly.
+        """
+        if sizes is not None:
+            self.__TABLE_SIZES = sizes
+
+        self.__size_index = 0
+        self.__array: ArrayR[tuple[str, V]] = ArrayR(self.__TABLE_SIZES[self.__size_index])
+        self.__length = 0
+        self.__hash_base = hash_base
 
     def hash(self, key: str) -> int:
         """
