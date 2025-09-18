68f07e67def683f2ec094a813709633368525772
Sean Silva <seansilva003@gmail.com>
2025-09-18 17:06:17 +1000

    Rename old max heap

--- tests/test_heaps.py
+++ tests/test_heaps.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from data_structures.array_heap import ArrayHeap
-from data_structures.max_array_heap import MaxArrayHeap
+from data_structures.max_array_heap_ import MaxArrayHeap
 from data_structures.min_array_heap import MinArrayHeap
 from data_structures.unordered_array_heap import UnorderedArrayHeap
 from data_structures.linked_heap import MinLinkedHeap
